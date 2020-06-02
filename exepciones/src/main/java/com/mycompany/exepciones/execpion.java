8cfa34f48a72dd6ef93bbd86c84643c256fa3902
jose eduardo ramirez mendoza <jose.ramirez1107@uppuebla.edu.mx>
2020-06-01 21:02:48 -0500

    tarea de exception

---
+++ exepciones/src/main/java/com/mycompany/exepciones/execpion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.exepciones;
+
+
+ 
+public class execpion {
+    
+   public static void main(String args[]){
+       int numerador , dedominador;
+       float divicion;
+       numerador=20;
+       dedominador=0;
+       System.out.println("Antes de la divicion");
+       divicion=numerador/dedominador;
+       
+       
+       System.out.println("Despues de la divicion");
+       
+   }
+
+}
---
+++ exepciones/src/main/java/exeption.java
@@ -0,0 +1,21 @@
+
+public class exeption{
+        public static void main(String args[]){
+       int numerador , dedominador;
+       float divicion;
+       numerador=20;
+       dedominador=0;
+       System.out.println("Antes de la divicion");
+       //try{
+       //divicion=numerador/dedominador;
+       //}catch (ArithmeticException e){
+           //System.out.println(e.getMessage());
+          //divicion=0;
+       //}
+        System.out.println("= "+divicion);
+       
+       System.out.println("Despues de la divicion");
+       
+   }
+
+}
